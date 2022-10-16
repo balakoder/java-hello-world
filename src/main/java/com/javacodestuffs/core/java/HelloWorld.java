d155c0c5626147c8d3bff3a46e4798dd8f4613e9
Bala.Koder <koder.bala@gmail.com>
2022-10-16 16:52:00 +0530

    initial code commit

---
+++ src/main/java/com/javacodestuffs/core/java/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.javacodestuffs.core.java;
+
+public class HelloWorld {
+
+	public static void main(final String[] args) {
+		System.out.println("Hello World ......... Welcome to Java Programming World!!!!!!!!!");
+	}
+}
